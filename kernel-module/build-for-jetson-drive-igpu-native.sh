14dc75dc8cc1b1b351c71edbaed93eb2c8f43bed
Hiteshkumar Patel <hiteshkumarg@nvidia.com>
2024-06-10 16:58:26 -0700

    Updated script for iGPU.
    - Remove build-for-igx-igpu_native.sh script.
    - Updated build-for-jetson-drive-igpu-native.sh

--- kernel-module/build-for-jetson-drive-igpu-native.sh
+++ kernel-module/build-for-jetson-drive-igpu-native.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2019-2024, NVIDIA CORPORATION. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -20,4 +20,26 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+SRC_DIR="$(find /usr/src/nvidia/nvidia-oot/* -name nv-p2p.h 2>/dev/null|head -1|xargs dirname 2>/dev/null)"
+if [ ! -d "${SRC_DIR}" ]; then
+	SRC_DIR="$(find /usr/src/./linux-headers-tegra-oot* -name nv-p2p.h 2>/dev/null|head -1|xargs dirname 2>/dev/null)"
+fi
+echo ${SRC_DIR}
+export NVIDIA_SRC_DIR="$(echo $(cd $SRC_DIR && cd ../ && pwd))"
+if [ ! -d "${NVIDIA_SRC_DIR}" ]; then
+        echo "ERROR: Could not find nv-p2p.h"
+        exit 1
+fi
+
+NV_EXTRA_SYMBOLS="$(find /usr/src/nvidia/nvidia-oot/* -name 'Module.symvers' |head -1)"
+if [ ! -f "${NV_EXTRA_SYMBOLS}" ]; then
+	NV_EXTRA_SYMBOLS="$(find  /usr/src/./linux-headers-tegra-oot* -name 'Module.symvers' |head -1)"
+fi
+
+echo ${NV_EXTRA_SYMBOLS}
+export NVIDIA_EXTRA_SYMBOLS=${NV_EXTRA_SYMBOLS}
+if [ ! -f "${NVIDIA_EXTRA_SYMBOLS}" ]; then
+        echo "ERROR: Could not find NVIDIA_EXTRA_SYMBOL"
+        exit 1
+fi
 exec make
